a45b1cd990e7ced3fc7556bb04c3b501d055c23e
Jan CW Kroeze <thejcwk@gmail.com>
2015-02-17 11:00:56 +0200

    Apply fibonacci patch.
    http://dwm.suckless.org/patches/fibonacci

--- config.def.h
+++ config.def.h
@@ -27,11 +27,14 @@ static const float mfact      = 0.50; /* factor of master area size [0.05..0.95]
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
 
+#include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[T]",      tile },    /* first entry is default */
 	{ "[F]",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
 };
 
 /* key definitions */
