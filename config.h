894e3656faeeb9b5f124736ae984f2c95e79fe7f
Jan CW Kroeze <thejcwk@gmail.com>
2015-02-17 11:31:26 +0200

    Make spiral default layout.

--- config.h
+++ config.h
@@ -30,10 +30,12 @@ static const Bool resizehints = True; /* True means respect size hints in tiled 
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[T]",      tile },    /* first entry is default */
-	{ "[F]",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+    /* first entry is default */
 	{ "[@]",      spiral },
+	{ "[T]",      tile },
+    /* no layout function means floating behavior */
+	{ "[F]",      NULL },
+	{ "[M]",      monocle },
 	{ "[\\]",     dwindle },
 };
 
@@ -70,11 +72,11 @@ static Key keys[] = {
     { MODKEY,                       XK_Return, zoom,           {0} },
     { MODKEY,                       XK_Tab,    view,           {0} },
     { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-    { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-    { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-    { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-    { MODKEY,                       XK_s,      setlayout,      {.v = &layouts[3]} },
-    { MODKEY,                       XK_d,      setlayout,      {.v = &layouts[4]} },
+    { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[1]} },
+    { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
+    { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[3]} },
+    { MODKEY,                       XK_s,      setlayout,      {.v = &layouts[4]} },
+    { MODKEY,                       XK_d,      setlayout,      {.v = &layouts[0]} },
     //{ MODKEY,                       XK_space,  setlayout,      {0} },
     //{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
     { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
