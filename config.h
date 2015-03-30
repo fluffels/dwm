f7a9de565142d95887e0802c3a21dfa3e5659a0f
Jan CW Kroeze <thejcwk@gmail.com>
2015-03-30 13:15:27 +0200

    Revert "Use local st instead of urxvt."
    This reverts commit a88773a47c04ac8780bba4e40c77950cd589d36f.

--- config.h
+++ config.h
@@ -52,7 +52,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "/home/jan/src/st/st", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 static const char *xlockcmd[] = { "slimlock", NULL };
 static const char *thunarcmd[] = { "thunar", NULL };
 
